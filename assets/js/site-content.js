09a0af88a33286914a925f5007f7859ed5919007
Denisov Evgenii <denisov.jeka@gmail.com>
2026-03-29 14:01:29 +0300

    Bump inline editor loader version again

--- assets/js/site-content.js
+++ assets/js/site-content.js
@@ -1,6 +1,6 @@
 (function() {
     const query = new URLSearchParams(window.location.search);
-    const INLINE_EDITOR_VERSION = '20260329-inline-toolbar-current';
+    const INLINE_EDITOR_VERSION = '20260329-inline-panel-save-visible';
 
     function shouldLoadInlineEditor() {
         return query.get('edit') === '1'
